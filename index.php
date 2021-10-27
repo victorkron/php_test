310c154aa446a4d44874d48c736fdeded05d9c7f
Karim <k.kron2015@yandex.ru>
2021-10-27 16:10:22 +0300

    first

---
+++ index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=width-device, user-scaleble=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <title>Лобовь есть соль жизни</title>
+  <link rel="stylesheet" href="./style/style.css">
+</head>
+<body>
+  <div class="header">
+    <img src="./image/handsome.jpg" class="avatar" alt="">
+    <div class="avatar_discription">If you are already crazy about this handsome guy, then look further... ♥</div>
+  </div>
+  <main class="main">
+    <div class="description">
+      <h2>Общая информация</h2>
+      <ul class="list">
+        <li>22 года</li>
+        <li>Рост: 180</li>
+        <li>Глаза: карие (душераздирающие)</li>
+        <li>Мать: Алкмена</li>
+        <li>Отец: Зевс</li>
+      </ul>
+      <div class="adding">
+        13 подвиг собираюсь совершить со своей возлюбленной
+      </div>
+      <div class="me">
+        Степень по радиофизике<br>
+        Спортивный<br>
+        Не пью, не курю<br>
+      </div>
+      <ul class="list">Любимые фильмы:
+        <li> "Фокус"</li>
+        <li>"Просто помиловать"</li>
+        <li>"Титаник"</li>
+      </ul>
+      <ul class="list">Любимая музыка (но в целом меломан):
+        <li>Соул</li>
+        <li>Джаз</li>
+        <li>Блюз</li>
+        <li>Рок-н-Ролл</li>
+        <li>Скриптонит</li>
+      </ul>
+      <div class="text">
+        <h3>Личностные качества:</h3>
+        Внимательный к деталям, добрый, умный, мудрый, амбициозный, надёжный, отважный,
+        галантный, честный, щедрый, стрессоустойчивый, мужественный, обладающий чувством юмора,
+        романтичный, верный, ценящий, умеренно ревнивый, ответственный и, конечно же, скромный.
+      </div>
+    </div>
+    <div class="review">
+      <h2>Рецензии</h2>
+      <div>
+        <ul class="list">
+          <li class="item">
+            "Ты лучшее, что было в моей жизни" <br> София Л.
+
+          </li>
+          <li class="item">
+            "Лучший парень месяца" <br> Оли И.
+          </li>
+          <li class="item">
+            Титул "главный не мудак 2020" <br> по версии Таня Е.
+          </li>
+        </ul>
+
+        <div class="link_wrapper">
+          <a href="https://vk.com/id_sencho" class="link"></a>
+        </div>
+
+
+
+      </div>
+    </div>
+
+  </main>
+  <?php
+  Copyright &copy; echo date("Y");
+  ?>
+</body>
+</html>
